edd251e694d3334a00bde4d7fdc58e99f67a07a7
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-07-15 13:49:08 -0400

    Eliminate imaginary part of index

--- lib/declare_parameters.m
+++ lib/declare_parameters.m
@@ -1,7 +1,7 @@
 thickness_of_first_layer = 15e-9;
 thickness_of_second_layer = 15e-9;
 refractive_index_of_first_layer = 2.1;
-refractive_index_of_second_layer = 7+0.45*1i;
+refractive_index_of_second_layer = 7;
 number_of_layers = 10;
 
 incident_wavevector = 2*pi/(465e-9);
