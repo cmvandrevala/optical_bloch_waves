d428b8eefd778c74d55c637292aa44d874ad6522
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-07-15 13:48:06 -0400

    Fix spelling error

--- lib/chebyshev_equality.m
+++ lib/chebyshev_equality.m
@@ -1,8 +1,10 @@
-function chebyshev_matrix = chebyshev_equality(transfer_matrix, number_of_layers)
+function chebyshev_matrix = chebyshev_equality(transfer_matrix)
 %   chebyshev_matrix = chebyshev_equality(transfer_matrix, number_of_layers)
 %   Calculate the total transfer matrix of a system using the Chebyshev identity
 %   transfer_matrix is the transfer matrix of a single unit cell
 %   number_of_layers is the number of layers of the material
+
+declare_parameters
 
 A = transfer_matrix(1,1);
 B = transfer_matrix(1,2);
@@ -10,8 +12,8 @@ C = transfer_matrix(2,1);
 D = transfer_matrix(2,2);
 N = number_of_layers;
 
-arguement = (A+D)/2;
-K_Lambda = acos(arguement);
+argument = (A+D)/2;
+K_Lambda = acos(argument);
 
 A_prime = (A*sin(N*K_Lambda) - sin((N-1)*K_Lambda))/sin(K_Lambda);
 B_prime = B*sin(N*K_Lambda)/sin(K_Lambda);
